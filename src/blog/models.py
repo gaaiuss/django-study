59df26c2485a3c0dacb230906c99912f3ce8ab9a
gaaiuss <caio.gui.castro@gmail.com>
2026-09-25 14:27:02 -0300

    add: foreign key from category and owner to post model

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -18,7 +19,18 @@ class Post(models.Model):
     short_description = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_date = models.DateTimeField(default=timezone.now)
-    categoty = models
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self) -> str:
         return self.title
